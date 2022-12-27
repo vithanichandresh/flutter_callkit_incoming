dd8739f64cb96a0aa13ecd54d7d82ae2e0e4733c
Vithani Chandresh <vithanichandresh.g@gmail.com>
2022-12-27 14:35:55 +0530

    comment app logo

--- android/src/main/kotlin/com/hiennv/flutter_callkit_incoming/CallkitNotificationManager.kt
+++ android/src/main/kotlin/com/hiennv/flutter_callkit_incoming/CallkitNotificationManager.kt
@@ -116,9 +116,9 @@ class CallkitNotificationManager(private val context: Context) {
         notificationBuilder.setContentIntent(getActivityPendingIntent(notificationId, data))
         notificationBuilder.setDeleteIntent(getTimeOutPendingIntent(notificationId, data))
         val typeCall = data.getInt(EXTRA_CALLKIT_TYPE, -1)
-        var smallIcon = context.applicationInfo.icon
+        var smallIcon = R.drawable.ic_video;//context.applicationInfo.icon
 //        if (typeCall > 0) {
-            smallIcon = R.drawable.ic_video
+//            smallIcon = R.drawable.ic_video
 //            smallIcon = R.drawable.ic_stat_name
 //        } else {
 //            if (smallIcon >= 0) {
